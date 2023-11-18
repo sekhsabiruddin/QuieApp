29c9f471931591a7d0f6aef369c7a00daa08e0f0
sabir <sksabiruddin81@gmail.com>
2023-11-18 18:26:35 +0530

    Committing changes in package files and removing some files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from "react";
 
 function App() {
+  const [apidata, SetApidata] = useState([]);
+  const [tracker, Settraker] = useState(0);
+  const APIURL = "https://jsonplaceholder.typicode.com/users";
+
+  const getData = async () => {
+    const reposnese = await fetch(APIURL);
+    const data = await reposnese.json();
+    SetApidata(data);
+  };
+  const sortTheData = () => {
+    if (tracker == 0 || tracker == 2) {
+      SetApidata([...apidata].sort((a, b) => a.name.length - b.name.length));
+      Settraker(1);
+    } else {
+      SetApidata([...apidata].sort((a, b) => b.name.length - a.name.length));
+      Settraker(2);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={(e) => getData()}>Get Data</button>
+      <button onClick={sortTheData}>Sort Data</button>
+      {apidata && apidata.map((ele, i) => <li key={i}>{ele.name}</li>)}
     </div>
   );
 }
